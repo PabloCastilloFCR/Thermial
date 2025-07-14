fdeeb50f4b30e75495844a4276e7c1fb6337ce02
Ricarda-Laura <ricarda.sack@fraunhofer.cl>
2025-07-14 15:59:51 -0400

    Módulo dissipador y wrapper sístema completa y export data completa

--- controlador (Pi 4)/custom code/i2c_0x15.py
+++ controlador (Pi 4)/custom code/i2c_0x15.py
@@ -2,32 +2,18 @@ import smbus2
 import time
 from cmd_dictionary import cmd_dict
 
-
-def send_command(PICO_ADDRESS, id, cmd, data=[], verbose=False):
-    """
-    Envía un comando al periferico.
-    :param id: ID único del comando. No importa
-    :param cmd: Código del comando (1 byte)
-    :param data: Datos adicionales (lista de bytes)
-    """
-    bus = smbus2.SMBus(1)  # Canal I2C en la Raspberry Pi 4
+def send_command(PICO_ADDRESS, id, cmd, data=[], verbose = True):
+    bus = smbus2.SMBus(1)  # I2C Bus der Raspberry Pi 4
     packet = [id, cmd, len(data)] + data
     bus.write_i2c_block_data(PICO_ADDRESS, 0x00, packet)
     cmd_str = "SET" if cmd == 0x01 else "GET"
     if verbose:
         print(f"Enviado: ADD={PICO_ADDRESS:02x}, CMD={cmd_str}, LEN={len(data)}, DATA={data}")
 
-
-def receive_response(PICO_ADDRESS, verbose=False) -> float:
-    """
-    Recibe la respuesta del periferico.
-    """
+def receive_response(PICO_ADDRESS, verbose = False):
     try:
-        fan_speed = None
-        bus = smbus2.SMBus(1)
-        data = bus.read_i2c_block_data(PICO_ADDRESS, 0x00, 4)  # ID, CMD, LEN=1, DATA
-        if verbose:
-            print(f"Datos recibidos sin procesar: {data}")
+        bus = smbus2.SMBus(1)  # I2C Bus der Raspberry Pi 4
+        data = bus.read_i2c_block_data(PICO_ADDRESS, 0x00, 8)
         response_id = data[0]
         response_cmd = data[1]
         response_len = data[2]
@@ -38,45 +24,57 @@ def receive_response(PICO_ADDRESS, verbose=False) -> float:
         else:
             response_cmd_str = "UNKNOWN"
 
-        if response_cmd == 0x16:  # cmd para ventilador
-            fan_speed = response_data[0]
+        if response_cmd == 0x12:  # Temperatur-Wert
+            if len(response_data) == 4:
+                temp5_value = response_data[0] + (response_data[1] << 8)
+                temp6_value = response_data[2] + (response_data[3] << 8)
+                temp5_value /= 100.0
+                temp6_value /= 100.0
+                if verbose:
+                    print(f"temperatura recibida: Temp5 = {temp5_value:.2f}°C, Temp6 = {temp6_value:.2f}°C")
+                return temp5_value, temp6_value
+            else:
+                print(f"Error: datos incompletos, esperando 4 bytes pero recibo: {response_len}: {response_data}")
+                return None
+
+        if response_cmd == 0x15:  # PWM-Wert des Ventilators
+            if len(response_data) == 1:
+                pwm_value = response_data[0]
             if verbose:
-                print(f"Velocidad recibida: {fan_speed}%")
-
-        if verbose:
-            print(f"Recibido: ADD={PICO_ADDRESS:02x}, CMD={response_cmd_str}, LEN={response_len}, DATA={response_data}")
-
-        return fan_speed
+                print(f"PWM recibido: {pwm_value}%")
+                print(f"Recibido: ADD={PICO_ADDRESS:02x}, CMD={response_cmd_str}, LEN={response_len}, DATA={pwm_value}")
+        return response_id, response_cmd, response_data
 
     except Exception as e:
-        print(f"Error al leer la respuesta: {e}")
+        if verbose:
+            print(f"Error al leer la respuesta: {e}")
         return None
 
-
 if __name__ == "__main__":
-    # uC = microcontrolador
-    PICO_ADDRESSES = 0x15
+    PICO_ADDRESSES = [0x15]  # I2C-Adresse des Ventilator-Moduls
     bus = smbus2.SMBus(1)
 
-    value = 70
-    increment = 5
+    value = 70       # PWM Startwert
+    increment = 5  # PWM Schrittgröße
 
-    while True:
-        # Enviar comando SET con el valor actual
-        send_command(PICO_ADDRESSES, 0, 0x01, [value])
-        time.sleep(0.5)
+    try:
+        while True:
+            for pico_add in PICO_ADDRESSES:
+                send_command(pico_add, 0, 0x02)  # GET Temperatur
+                time.sleep(0.5)
+                receive_response(pico_add, verbose=True)
+                time.sleep(0.5)
+                send_command(pico_add, 0, 0x01, [value])  # SET PWM
+                time.sleep(1)
 
-        # Enviar comando GET
-        send_command(PICO_ADDRESSES, 0, 0x02)
-        time.sleep(0.5)
-
-        # Leer la respuesta del dispositivo
-        receive_response(PICO_ADDRESSES)
-
-        time.sleep(0.5)
-
-        # Incrementar o decrementar el valor
-        value += increment
-        if value > 100 or value < 70:
-            increment = -increment
             value += increment
+            if value > 100 or value < 70:
+                increment = -increment
+                value += increment
+
+    except KeyboardInterrupt:
+        print("Ventilador está siendo apagado")
+        for pico_add in PICO_ADDRESSES:
+            send_command(pico_add, 0, 0x01, [0], verbose=True)
+            time.sleep(0.2) 
+        print("Ventilador apagado.")     
---
+++ controlador (Pi 4)/custom code/modulo_estanque_loop_termico.py
@@ -0,0 +1,52 @@
+import i2c_0x13
+import time
+
+class Estanque:
+    def __init__(self):
+        self.address = 0x13
+        self.device_name = "Modulo Estanque"
+        self.nivel = None
+        self.temp3 = 0
+        self.temp4 = 0
+
+
+    def get_temperaturas(self):
+        """
+        Solicita y devuelve los dos valores de los sensores de temperatura.
+        :return: (t1, t2) ó ninguno cuando ocurre un error
+        """
+        i2c_0x13.send_command(self.address, 0, 0x02)
+        time.sleep(0.5)
+        response = i2c_0x13.receive_response(self.address)
+        if response is None:
+            return None
+        response_data = response
+
+        if len(response_data) >= 4:
+            temp3 = (response_data[0] | (response_data[1] << 8)) / 100.0
+            temp4 = (response_data[2] | (response_data[3] << 8)) / 100.0
+            self.temp3 = temp3
+            self.temp4 = temp4
+            print(f"temperatura recibida: Temp3 = {temp3:.2f}°C, Temp4 = {temp4:.2f}°C")
+        else:
+            self.temp3 = -1
+            self.temp4 = -1
+
+    def get_nivel(self):
+        """
+        Solicita y devuelve el nivel actual del estanque.
+        :return: Nivel actual del estanque.
+        """
+        i2c_0x13.send_command(self.address, 0, 0x03)
+        time.sleep(0.5)
+        response = i2c_0x13.receive_response(self.address)
+        if response is None:
+            return None
+        response_data = response
+
+        if len(response_data) >= 2:
+            nivel = response_data[0] | (response_data[1] << 8)
+            self.nivel = nivel
+            print(f"nivel recibido: {nivel:.1f} cm")
+        else:
+            self.nivel = -1
